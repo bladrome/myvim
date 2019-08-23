0b5fa596a141d46ba8ef71c73de5c1aea186f168
blkcat <blackwhitedoggie@163.com>
2019-08-24 00:07:42 +0800

    Add vim-plug command

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@
 # ROLE:     install myvim
 cp .vimrc ~/.vimrc
 cp .vim ~/.vim -a
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +BundleInstall +qall
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PlugInstall +qall
