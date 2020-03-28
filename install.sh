91061a6e08a094aeb52dad4e183274645d2c03a1
bladrome <blackwhitedoggie@163.com>
2020-03-28 11:43:47 +0800

    nvim

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 # AUTHOR:   bladrome
 # FILE:     install.sh
 # ROLE:     install myvim
-cp .vimrc ~/.vimrc
-cp .vim ~/.vim -a
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PlugInstall +qall
+cp init.vim ~/.config/nvim/
+pip install -U pynvim
