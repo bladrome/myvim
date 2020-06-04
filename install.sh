69c46e26254afc8759bb5bce0d9eaf1a513c95b8
blkcat <blackwhitedoggie@163.com>
2020-06-04 16:03:14 +0800

    typo node

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp init.vim ~/.config/nvim/
-yay -S cscope node
+yay -S cscope nodejs
 pip install -U pynvim
