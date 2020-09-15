77445742029203d45a060a0ed693c46e993d59f4
bladrome <blackwhitedoggie@163.com>
2020-09-15 11:08:29 +0800

    fix, we can not download directly for raw.github.*

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # FILE:     install.sh
 # ROLE:     install myvim
 
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.config/nvim/
+git clone --depth=1 https://github.com/junegunn/vim-plug.git ~/.config/nvim/autoload
 cp init.vim ~/.config/nvim/
 yay -S cscope nodejs
 pip install -U pynvim
